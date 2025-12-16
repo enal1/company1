84138a193cfad83882e4ebfdd2d04d2919470b4a
石鹏钊 <1985656542@qq.com>
2025-12-16 11:05:24 +0800

    chore: ready for static export

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  reactStrictMode: true,
+  output: 'export',   // ← 关键：启用静态导出
+  distDir: 'out',     // ← 可选：指定输出目录，默认就是 out
   images: {
     remotePatterns: [
       {
